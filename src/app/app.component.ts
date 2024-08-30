c08ebd10a6ce5189525eafad4315949683aa9c2a
TanchanokMay <89119756+MayJarung@users.noreply.github.com>
2024-08-30 15:39:58 +0700

    create basic footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 import { User } from './models/User.model';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
 	standalone: true,
 	selector: 'app-root',
 
-	imports: [NavbarComponent],
+	imports: [NavbarComponent, FooterComponent],
 
 	templateUrl: './app.component.html',
 })
