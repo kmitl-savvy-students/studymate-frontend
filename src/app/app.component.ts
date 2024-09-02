c9d26b990ed623fdf393f8ad7ea7757736cd4b99
TanchanokMay <89119756+MayJarung@users.noreply.github.com>
2024-09-03 03:27:19 +0700

    update  routing for signup/signin page, update signup, signin, footer, navbar, let-plan and responsive for xl and 2xl device

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { User } from './models/User.model';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { SignUpComponent } from "./components/sign-up/sign-up.component";
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @Component({
 	standalone: true,
 	selector: 'app-root',
 
 	imports: [
-    NavbarComponent,
-    FooterComponent,
-    RouterOutlet,
-    SignUpComponent
-],
+		RouterOutlet,
+		NavbarComponent,
+		FooterComponent,
+		SignUpComponent,
+	],
 
 	templateUrl: './app.component.html',
 })
@@ -24,4 +24,6 @@ export class AppComponent {
 	addUser() {
 		this.users.push(new User('asd', 'asd'));
 	}
+
+	constructor(public router: Router) {}
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-navbar',
 	standalone: true,
-	imports: [RouterLink, RouterLinkActive],
+	imports: [RouterLink],
 	templateUrl: './navbar.component.html',
 	styleUrl: './navbar.component.css',
 })
