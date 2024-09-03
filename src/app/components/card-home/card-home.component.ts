204d752a30b325dddad9335150e8c5bed4a4138e
TanchanokMay <89119756+MayJarung@users.noreply.github.com>
2024-09-03 20:40:36 +0700

    update home component

---
+++ src/app/components/card-home/card-home.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-home.component.html',
+  styleUrl: './card-home.component.css'
+})
+export class CardHomeComponent {
+  @Input() headerText: string = '';
+  @Input() image: string = '';
+  @Input() buttonText: string = '';
+}
