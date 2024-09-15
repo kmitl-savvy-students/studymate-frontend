ab52129bec8819b830d2e982f57859fa73b8fcf1
kornyellow <kornkubzaza@gmail.com>
2024-09-15 07:24:12 +0700

    Added component google button

--- src/app/pages/sign-up/sign-up.page.ts
+++ src/app/pages/sign-up/sign-up.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SDMButtonGoogle } from '../../components/buttons/google/button-google.component';
 
 @Component({
 	selector: 'sdm-page-sign-up',
 	standalone: true,
-	imports: [RouterLink],
+	imports: [RouterLink, SDMButtonGoogle],
 	templateUrl: './sign-up.page.html',
 	styleUrl: './sign-up.page.css',
 })
