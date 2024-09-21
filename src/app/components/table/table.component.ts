8b12a512594c90bcd9e676aa4e3797e101d629d2
TanchanokMay <89119756+MayJarung@users.noreply.github.com>
2024-09-21 17:58:58 +0700

    update this page

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { IconComponent } from '../icon/icon.component';
 
 @Component({
 	selector: 'sdm-table',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, IconComponent],
 	templateUrl: './table.component.html',
 	styleUrl: './table.component.css',
 })
 export class TableComponent {
 	@Input() listThead: string[] = [];
+	isExpanded1: boolean = true;
+	isExpanded2: boolean = true;
+	toggleExpand1() {
+		this.isExpanded1 = !this.isExpanded1;
+	}
+	toggleExpand2() {
+		this.isExpanded2 = !this.isExpanded2;
+	}
 }
