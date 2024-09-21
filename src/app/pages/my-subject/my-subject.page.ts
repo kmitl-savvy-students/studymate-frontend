82db9ae8e61d887cfa1fb110a60042d81d295ac5
TanchanokMay <89119756+MayJarung@users.noreply.github.com>
2024-09-21 21:49:39 +0700

    update this page

--- src/app/pages/my-subject/my-subject.page.ts
+++ src/app/pages/my-subject/my-subject.page.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { TableComponent } from '../../components/table/table.component';
 import { CreditDashboardComponent } from '../../components/credit-dashboard/credit-dashboard.component';
+import { AdviceDashboardComponent } from '../../components/advice-dashboard/advice-dashboard.component';
 
 @Component({
 	selector: 'sdm-my-subject',
 	standalone: true,
-	imports: [TableComponent, CreditDashboardComponent],
+	imports: [
+		TableComponent,
+		CreditDashboardComponent,
+		AdviceDashboardComponent,
+	],
 	templateUrl: './my-subject.page.html',
 	styleUrl: './my-subject.page.css',
 })
