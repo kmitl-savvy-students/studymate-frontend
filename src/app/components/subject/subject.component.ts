f301cb309bf0faf209d6fb27f6945c4b1a083192
Tanchanok Charungpattananon <89119756+MayJarung@users.noreply.github.com>
2024-12-05 01:45:58 +0700

    create subject component simple

---
+++ src/app/components/subject/subject.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IconComponent } from '../icon/icon.component';
+
+@Component({
+	selector: 'sdm-subject-cpn',
+	standalone: true,
+	imports: [IconComponent],
+	templateUrl: './subject.component.html',
+	styleUrl: './subject.component.css',
+})
+export class SDMSubjectComponent {}
