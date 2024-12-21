0e1222f787ca5533608af7bd72c5bf1a98462d75
Tanchanok Charungpattananon <89119756+MayJarung@users.noreply.github.com>
2024-12-21 23:48:43 +0700

    ีupdate(): add rating star component

--- src/app/components/subject-review/subject-review.component.ts
+++ src/app/components/subject-review/subject-review.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IconComponent } from '../icon/icon.component';
+import { SDMRatingComponent } from '../rating/rating.component';
 
 @Component({
 	selector: 'sdm-subject-review',
 	standalone: true,
-	imports: [IconComponent],
+	imports: [IconComponent, SDMRatingComponent],
 	templateUrl: './subject-review.component.html',
 	styleUrl: './subject-review.component.css',
 })
