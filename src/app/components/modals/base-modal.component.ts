7e4b0ddc87c6a691e76fb34596f320a6ff92c5b7
Thanatat Chongkittisakul <thanatat64@gmail.com>
2025-02-18 02:56:15 +0700

    update(): fix admin ui

--- src/app/components/modals/base-modal.component.ts
+++ src/app/components/modals/base-modal.component.ts
@@ -8,10 +8,7 @@ import { SDMBaseButton } from '../buttons/base-button.component';
 	imports: [CommonModule, SDMBaseButton],
 	standalone: true,
 	template: `
-		<div
-			[id]="modalId"
-			tabindex="-1"
-			class="fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden md:inset-0">
+		<div [id]="modalId" tabindex="-1" class="fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden md:inset-0">
 			<div class="relative max-h-full w-full max-w-xl p-4">
 				<div class="relative rounded-lg bg-white shadow">
 					<div class="flex items-center justify-between rounded-t border-b p-4 md:p-5">
@@ -22,40 +19,34 @@ import { SDMBaseButton } from '../buttons/base-button.component';
 					<div class="p-4 md:p-5">
 						<ng-content />
 						<div class="basis flex gap-2">
-							<ng-container *ngIf="!normalModal; else showNormal">
+							<ng-container *ngIf="!normalModal && !turnbackModal">
 								<div class="w-1/2">
-									<sdm-base-button
-										icon="xmark"
-										text="ยกเลิก"
-										textColor="text-light"
-										textColorHover="text-light"
-										backgroundColor="bg-danger-300"
-										backgroundColorHover="hover:bg-danger-400"
-										(clickEvent)="handleCancel()" />
+									<sdm-base-button icon="xmark" text="ยกเลิก" textColor="text-light" textColorHover="text-light" backgroundColor="bg-danger-300" backgroundColorHover="hover:bg-danger-400" (clickEvent)="handleCancel()" />
 								</div>
 								<div class="w-1/2">
-									<sdm-base-button
-										icon="check"
-										text="ตกลง"
-										textColor="text-light"
-										textColorHover="text-light"
-										backgroundColor="bg-primary-300"
-										backgroundColorHover="hover:bg-primary-400"
-										(clickEvent)="handleConfirm()" />
+									<sdm-base-button icon="check" text="ตกลง" textColor="text-light" textColorHover="text-light" backgroundColor="bg-primary-300" backgroundColorHover="hover:bg-primary-400" (clickEvent)="handleConfirm()" />
 								</div>
 							</ng-container>
-							<ng-template #showNormal>
+							<ng-container *ngIf="!turnbackModal && normalModal">
 								<div class="w-full">
-									<sdm-base-button
-										icon="xmark"
-										text="ปิด"
-										textColor="text-black"
-										textColorHover="text-light"
-										backgroundColor="bg-gray-300"
-										backgroundColorHover="hover:bg-gray-400"
-										(clickEvent)="handleCancel()" />
+									<sdm-base-button icon="xmark" text="ปิด" textColor="text-black" textColorHover="text-light" backgroundColor="bg-gray-300" backgroundColorHover="hover:bg-gray-400" (clickEvent)="handleCancel()" />
 								</div>
-							</ng-template>
+							</ng-container>
+							<ng-container *ngIf="!normalModal && turnbackModal">
+								<div class="w-1/2">
+									<sdm-base-button
+										icon="right-to-bracket"
+										text="ย้อนกลับ"
+										textColor="text-light"
+										textColorHover="text-light"
+										backgroundColor="bg-main-100"
+										backgroundColorHover="hover:bg-main-200"
+										(clickEvent)="handleConfirm()" />
+								</div>
+								<div class="w-1/2">
+									<sdm-base-button icon="xmark" text="ปิด" textColor="text-black" textColorHover="text-light" backgroundColor="bg-gray-300" backgroundColorHover="hover:bg-gray-400" (clickEvent)="handleCancel()" />
+								</div>
+							</ng-container>
 						</div>
 					</div>
 				</div>
@@ -67,6 +58,7 @@ export class SDMBaseModal implements AfterViewInit {
 	@Input() modalId: string = '';
 	@Input() header: string = '';
 	@Input() normalModal: boolean = false;
+	@Input() turnbackModal: boolean = false;
 
 	@Output() confirmEvent = new EventEmitter<void>();
 	@Output() cancelEvent = new EventEmitter<void>();
