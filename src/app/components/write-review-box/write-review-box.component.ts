41ae85f3a79ebb42065087e301b011ae30920472
Tanchanok Charungpattananon <89119756+MayJarung@users.noreply.github.com>
2025-02-21 07:56:58 +0700

    update(): user can review if user have grade in transcript (not X), if user cannot review show warning, if user can review hide year and term and show text from transcript

--- src/app/components/write-review-box/write-review-box.component.ts
+++ src/app/components/write-review-box/write-review-box.component.ts
@@ -1,30 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { semesterList, yearsList } from '../../pages/subjects/subject-page-data.js';
-import { SelectedData } from '../../shared/models/SdmAppService.model.js';
 import { User } from '../../shared/models/User.model.js';
 import { SDMButtonLink } from '../buttons/button-link.component';
 import { IconComponent } from '../icon/icon.component';
 import { SDMRatingComponent } from '../rating/rating.component';
 import { SDMRichTextEditor } from '../rich-text-editor/rich-text-editor.component';
-import { SDMSelectComponent } from '../select/select.component';
 
 @Component({
 	selector: 'sdm-write-review-box',
 	standalone: true,
-	imports: [CommonModule, IconComponent, SDMSelectComponent, SDMRatingComponent, SDMRichTextEditor, SDMButtonLink],
+	imports: [CommonModule, IconComponent, SDMRatingComponent, SDMRichTextEditor, SDMButtonLink],
 	templateUrl: './write-review-box.component.html',
 	styleUrl: './write-review-box.component.css',
 })
 export class SDMWriteReviewBoxComponent {
 	@Input() isEdit: boolean = false;
 	@Input() signedIn: boolean = false;
+	@Input() hasCompletedSubject: boolean = false;
 	@Input() currentUser: User | null = null;
 	@Input() subjectId!: string;
 	@Input() editReviewContent: string = '';
+	@Input() completedSubjectDetails: { year: number | null; term: number | null } | null = null;
+	@Input() canReview: boolean = false;
+	@Input() notCompletedSubject: boolean = false;
+	@Input() notHaveTranscript: boolean = false;
+
 	@Output() reviewSuccess = new EventEmitter<void>();
 	@Output() confirmEditReview = new EventEmitter<void>();
 	@Output() cancelEditReview = new EventEmitter<void>();
+
 	public selectedYear?: number = 0;
 	public selectedSemester?: number = 0;
 
@@ -39,34 +44,11 @@ export class SDMWriteReviewBoxComponent {
 	public reviewRating: number = 0;
 
 	public onMarkdownChange(content: string): void {
-		this.markdownContent = content; // รับค่าจาก Markdown Editor
+		this.markdownContent = content;
 	}
-
-	public handleSelectChange(selectName: string, selectedData: SelectedData) {
-		switch (selectName) {
-			case 'selectedYear':
-				this.selectedYear = selectedData.value;
-				break;
-			case 'selectedSemester':
-				this.selectedSemester = selectedData.value;
-				break;
-		}
-	}
-
-	public updatePermission() {}
 
 	public onRatingChange(rating: number) {
 		this.reviewRating = rating;
-	}
-
-	public checkSelectAllDropdown() {
-		if (this.selectedYear && this.selectedSemester) {
-			this.isSelectAllDropdown = true;
-		} else {
-			this.isSelectAllDropdown = false;
-		}
-
-		console.log('isSelectAllDropdown', this.isSelectAllDropdown);
 	}
 
 	public resetForm(): void {
--- src/app/pages/subject-detail/subject-detail.page.ts
+++ src/app/pages/subject-detail/subject-detail.page.ts
@@ -1,8 +1,14 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, OnInit, SimpleChanges } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Subject } from '@models/Subject.model';
+import { Transcript } from '@models/Transcript.model';
+import { AlertService } from '@services/alert/alert.service';
+import { BackendService } from '@services/backend.service';
+import { LoadingService } from '@services/loading/loading.service';
 import { initFlowbite } from 'flowbite';
+import { finalize } from 'rxjs';
 import { SDMRatingComponent } from '../../components/rating/rating.component';
 import { SDMReviewFilterComponent } from '../../components/review-filter/review-filter.component';
 import { SDMShowSubjectsOpenComponent } from '../../components/show-subjects-open/show-subjects-open.component';
@@ -30,6 +36,7 @@ export class SDMPageSubjectDetail implements OnInit, AfterViewInit {
 
 	public signedIn: boolean = false;
 	public currentUser: User | null = null;
+	public transcript: Transcript | null = null;
 
 	public selectedYear: number = -1;
 	public selectedSemester: number = -1;
@@ -42,9 +49,12 @@ export class SDMPageSubjectDetail implements OnInit, AfterViewInit {
 
 	constructor(
 		private route: ActivatedRoute,
-		private router: Router,
 		private apiManagementService: APIManagementService,
 		private authService: AuthenticationService,
+		private http: HttpClient,
+		private backendService: BackendService,
+		private alertService: AlertService,
+		private loadingService: LoadingService,
 	) {}
 
 	ngOnChanges(changes: SimpleChanges): void {
@@ -58,41 +68,112 @@ export class SDMPageSubjectDetail implements OnInit, AfterViewInit {
 		});
 		this.authService.user$.subscribe((user) => {
 			this.currentUser = user;
+			this.route.params.subscribe((params) => {
+				this.selectedYear = +params['year'];
+				this.selectedSemester = +params['semester'];
+				this.selectedCurriculum = +params['curriculum'];
+				this.section = +params['section'];
+				this.subjectId = params['subjectId'];
+
+				if (
+					(this.selectedYear === -1 || isNaN(this.selectedYear)) &&
+					(this.selectedSemester === -1 || isNaN(this.selectedSemester)) &&
+					(this.selectedCurriculum === -1 || isNaN(this.selectedCurriculum)) &&
+					(this.section === -1 || isNaN(this.section)) &&
+					(this.subjectId !== '' || this.subjectId !== undefined)
+				) {
+					this.getSubjectsDataBySubjectId();
+				} else if (!isNaN(this.selectedYear) && !isNaN(this.selectedSemester) && !isNaN(this.selectedCurriculum) && this.subjectId !== '' && !isNaN(this.section)) {
+					this.getEachSubjectData();
+				}
+				this.fetchTranscripts();
+				this.getSubjectReviews();
+			});
 		});
-
-		this.route.params.subscribe((params) => {
-			this.selectedYear = +params['year'];
-			this.selectedSemester = +params['semester'];
-			this.selectedCurriculum = +params['curriculum'];
-			this.section = +params['section'];
-			this.subjectId = params['subjectId'];
-
-			console.log('From Subject Detail Page');
-			console.log(`
-				selectedYear = ${this.selectedYear},
-				selectedSemester = ${this.selectedSemester},
-				selectedCurriculum = ${this.selectedCurriculum},
-				section = ${this.section},
-				subjectId = ${this.subjectId}
-			`);
-
-			if (
-				(this.selectedYear === -1 || isNaN(this.selectedYear)) &&
-				(this.selectedSemester === -1 || isNaN(this.selectedSemester)) &&
-				(this.selectedCurriculum === -1 || isNaN(this.selectedCurriculum)) &&
-				(this.section === -1 || isNaN(this.section)) &&
-				(this.subjectId !== '' || this.subjectId !== undefined)
-			) {
-				this.getSubjectsDataBySubjectId();
-			} else if (!isNaN(this.selectedYear) && !isNaN(this.selectedSemester) && !isNaN(this.selectedCurriculum) && this.subjectId !== '' && !isNaN(this.section)) {
-				this.getEachSubjectData();
-			}
-			this.getSubjectReviews();
-		});
+		console.log('currentUser in ngOnInIt : ', this.currentUser);
 	}
 
 	ngAfterViewInit(): void {
 		initFlowbite();
+	}
+
+	fetchTranscripts() {
+		console.log('currentUser in fetch Transcript : ', this.currentUser);
+		if (!this.currentUser) return;
+
+		const apiUrl = `${this.backendService.getBackendUrl()}/api/transcript/get-by-user/${this.currentUser.id}`;
+		this.http
+			.get<Transcript>(apiUrl)
+			.pipe(
+				finalize(() => {
+					this.loadingService.hide();
+				}),
+			)
+			.subscribe({
+				next: (data) => {
+					this.transcript = data;
+					if (data) {
+						this.hasTranscript = true;
+					} else {
+						this.hasTranscript = false;
+					}
+					this.checkReviewSubject();
+					this.updateWriteReviewPermission();
+					console.log('333333333333333333333333333333333333333333333');
+				},
+				error: (error) => {
+					console.error('Error fetching transcript:', error);
+					this.hasTranscript = false;
+				},
+			});
+	}
+
+	public hasTranscript: boolean = false;
+	public hasCompletedSubject: boolean = false;
+	public completedSubjectDetails: { year: number | null; term: number | null } | null = null;
+
+	public checkReviewSubject() {
+		if (!this.transcript || !this.currentUser) {
+			this.hasCompletedSubject = false;
+			this.completedSubjectDetails = null;
+			console.log('hasCompletedSubject : ', this.hasCompletedSubject);
+			return;
+		}
+		const completedSubject = this.transcript.details.find((detail) => detail.subject?.id === this.subjectId && detail.grade && detail.grade.toUpperCase() !== 'X');
+
+		if (completedSubject) {
+			this.hasCompletedSubject = true;
+			this.completedSubjectDetails = {
+				year: completedSubject.teachtable?.year ? completedSubject.teachtable.year + 543 : null,
+				term: completedSubject.teachtable?.term ?? null,
+			};
+			if (!this.completedSubjectDetails?.year && !this.completedSubjectDetails?.term) {
+				this.completedSubjectDetails = null;
+			}
+		} else {
+			this.hasCompletedSubject = false;
+			this.completedSubjectDetails = null;
+		}
+
+		console.log('hasCompletedSubject : ', this.hasCompletedSubject);
+		console.log('completedSubjectDetails : ', this.completedSubjectDetails);
+	}
+
+	public canReview: boolean = false;
+	public notCompletedSubject: boolean = false;
+	public notHaveTranscript: boolean = false;
+
+	public updateWriteReviewPermission() {
+		if (this.signedIn && this.hasTranscript && this.hasCompletedSubject && this.completedSubjectDetails) {
+			this.canReview = true;
+		} else if (this.signedIn && this.hasTranscript && !this.hasCompletedSubject) {
+			this.notCompletedSubject = true;
+		} else if (this.signedIn && !this.hasTranscript) {
+			this.notHaveTranscript = true;
+		}
+		console.log('canReview : ', this.canReview);
+		console.log('notCompletedSubject : ', this.notCompletedSubject);
+		console.log('notHaveTranscript : ', this.notHaveTranscript);
 	}
 
 	get paginationType() {
