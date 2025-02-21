f75a5536008f41832ce1bcf08fa1084cbedb68c1
Thanatat Chongkittisakul <thanatat64@gmail.com>
2025-02-21 07:43:14 +0700

    update():filter bar with accordion

--- src/app/components/filter-bar/filter-bar.component.ts
+++ src/app/components/filter-bar/filter-bar.component.ts
@@ -5,13 +5,15 @@ import { User } from '@models/User.model.js';
 import { APIManagementService } from '@services/api-management.service.js';
 import { AuthenticationService } from '@services/authentication/authentication.service.js';
 import { LoadingService } from '@services/loading/loading.service.js';
+import { SDMBaseAccordion } from '../accordion/base-accordion.component';
 import { IconComponent } from '../icon/icon.component';
+import { SDMLoadingSkeletonComponent } from '../loading-skeleton/loading-skeleton.component';
 import { CurriculumGroup } from './../../shared/models/CurriculumGroup.model';
 
 @Component({
 	selector: 'sdm-filter-bar',
 	standalone: true,
-	imports: [IconComponent, CommonModule],
+	imports: [IconComponent, CommonModule, SDMLoadingSkeletonComponent, SDMBaseAccordion],
 	templateUrl: './filter-bar.component.html',
 	styleUrl: './filter-bar.component.css',
 })
@@ -27,7 +29,9 @@ export class SDMfilterBarComponent implements OnInit {
 	public transcript: Transcript | null = null;
 	public isLoadingTranscript: boolean = false;
 	public curriculumGroup: Array<CurriculumGroup> | undefined = [];
-	// renderGroup: TemplateRef<{ $implicit: CurriculumGroup[]|undefined; level: number; }>|null;
+	public openAccordions: Set<number> = new Set<number>();
+	public accordionLevelExpands: number = 2;
+	public rootNode: CurriculumGroup | null = null;
 
 	constructor(
 		private apiManagementService: APIManagementService,
@@ -38,8 +42,58 @@ export class SDMfilterBarComponent implements OnInit {
 	ngOnInit(): void {
 		this.authService.user$.subscribe((user) => {
 			this.currentUser = user;
+			if (this.currentUser?.curriculum?.curriculum_group) {
+				this.openAccordions.add(this.currentUser.curriculum.curriculum_group.id);
+				this.expandAccordions(this.currentUser.curriculum.curriculum_group, this.accordionLevelExpands - 1);
+			}
 			this.fetchTranscripts();
 		});
+	}
+
+	findParentNodeColor(currentNode: CurriculumGroup): string {
+		const GRAY = '#e7e5e4';
+		if (currentNode.color?.toUpperCase() !== '#FFFFFF' && currentNode.color?.trim().length !== 0) {
+			return currentNode.color;
+		}
+		const parentNode = this.findNodeById(currentNode.parent_id, this.rootNode);
+		if (!parentNode) return GRAY;
+		return this.findParentNodeColor(parentNode);
+	}
+
+	findNodeById(id: number, currentNode: CurriculumGroup | null): CurriculumGroup | null {
+		if (!currentNode) return null;
+		if (currentNode.id === id) return currentNode;
+		for (let child of currentNode.children) {
+			const result = this.findNodeById(id, child);
+			if (result) return result;
+		}
+		return null;
+	}
+
+	expandAccordions(group: CurriculumGroup, levelLeft: number): void {
+		if (levelLeft === 0) return;
+		if (group.children && group.children.length > 0) {
+			group.children.forEach((child) => {
+				this.openAccordions.add(child.id);
+				this.expandAccordions(child, levelLeft - 1);
+			});
+		}
+	}
+
+	collapseAllAccordions(): void {
+		this.openAccordions.clear();
+	}
+
+	toggleAccordion(groupId: number) {
+		if (this.openAccordions.has(groupId)) {
+			this.openAccordions.delete(groupId);
+		} else {
+			this.openAccordions.add(groupId);
+		}
+	}
+
+	isAccordionOpen(groupId: number): boolean {
+		return this.openAccordions.has(groupId);
 	}
 
 	fetchTranscripts() {
@@ -48,6 +102,7 @@ export class SDMfilterBarComponent implements OnInit {
 		this.apiManagementService.FetchTranscript(this.currentUser.id).subscribe({
 			next: (data) => {
 				this.transcript = data;
+				this.rootNode = data.user!.curriculum.curriculum_group;
 				this.curriculumGroup = data.user!.curriculum.curriculum_group?.children;
 				this.isLoadingTranscript = false;
 				console.log('filter data:', this.transcript);
